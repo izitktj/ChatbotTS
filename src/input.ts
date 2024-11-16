258758a91c61c5e3d59dfd23a871c6c1b4744357
izitktj <universoreversoizi@gmail.com>
2024-11-15 21:48:14 -0300

    Adicionado input de texto para o chatbot

---
+++ src/input.ts
@@ -0,0 +1,17 @@
+const readline = require('node:readline');
+const { stdin: input, stdout: output } = require('node:process');
+
+const rl = readline.createInterface({ input, output });
+
+export function readLine(): Promise<string> {
+    return new Promise((resolve) => {
+    const rl = readline.createInterface({
+      input: process.stdin
+    });
+
+    rl.question("", (resposta: string) => {
+      rl.close();
+      resolve(resposta);
+    });
+  });
+}
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,17 @@
 import { groq } from "./groqRequest";
+import { readLine } from "./input";
 
 const groqInterface = new groq();
 
-async function main() {
-	const response = await groqInterface.sendMessage("user", "Hi");
+const running = true;
 
-	console.log("Chat: ", response);
+async function main() {
+	while(running) {
+		process.stdout.write("\nYou: ");
+		const response = await groqInterface.sendMessage("user", await readLine());
+
+		console.log("\nChat: ", response);
+	}
 }
 
 main();
