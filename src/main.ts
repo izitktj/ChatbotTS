dabe483fddf510f7bc2a9b7b514285d07076a00e
Gustavo Almeida <universoreversoizi@gmail.com>
2024-11-23 20:28:50 +0000

    Added chat history

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { groq } from "./groqRequest";
 import { readLine } from "./input";
 import { styleText } from "./styleText";
 
-const groqInterface = new groq();
+const groqInterface = new groq(groq.LLMS["GEMMA2"]);
 
 var running = true;
 
@@ -13,6 +13,16 @@ async function main() {
 
 		styleText.printStyledText("\n**Chat:** " + response);
 	}
+}
+
+function getChatHistory(): string {
+	let chatHistory: string = "";
+
+	groqInterface.chatHistory.forEach(element => {
+		chatHistory += "\n{ Role: " + element.role + ", Message: " + element.content + " }";
+	});
+
+	return chatHistory;
 }
 
 main();
